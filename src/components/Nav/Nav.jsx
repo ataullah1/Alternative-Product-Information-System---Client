66bdb41646ec445dab53bc743c75b0ee8df6c514
Md Ataullah <ataullahm100@gmail.com>
2024-05-10 11:00:51 +0600

    Navigation adding

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,31 +1,44 @@
 import { Link } from 'react-router-dom';
-
+import logo from '../../assets/finalLogo.png';
 const Nav = () => {
   return (
     <div>
-      <ul className="flex items-center justify-around w-2/3 gap-12 mx-auto">
-        <Link className="py-2 px-3 bg-slate-800 text-white rounded-md" to={'/'}>
-          Home
-        </Link>
-        <Link
-          className="py-2 px-3 bg-slate-800 text-white rounded-md"
-          to={'/about'}
-        >
-          About
-        </Link>
-        <Link
-          className="py-2 px-3 bg-slate-800 text-white rounded-md"
-          to={'/login'}
-        >
-          Login
-        </Link>
-        <Link
-          className="py-2 px-3 bg-slate-800 text-white rounded-md"
-          to={'/register'}
-        >
-          Register
-        </Link>
-      </ul>
+      <div className="w-11/12 mx-auto flex items-center justify-between">
+        <div>
+          <img src={logo} alt="" className="h-16" />
+        </div>
+        <ul className="flex items-center justify-around w-2/3 gap-12 mx-auto">
+          <Link
+            className="py-2 px-3 bg-slate-800 text-white rounded-md"
+            to={'/'}
+          >
+            Home
+          </Link>
+          <Link
+            className="py-2 px-3 bg-slate-800 text-white rounded-md"
+            to={'/about'}
+          >
+            About
+          </Link>
+          <Link
+            className="py-2 px-3 bg-slate-800 text-white rounded-md"
+            to={'/login'}
+          >
+            Login
+          </Link>
+          <Link
+            className="py-2 px-3 bg-slate-800 text-white rounded-md"
+            to={'/register'}
+          >
+            Register
+          </Link>
+        </ul>
+        <div>
+          <button className="py-2 w-24 bg-mainColor rounded-md text-white font-bold">
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: { mainColor: '#38B6FF' },
+    },
   },
   // plugins: [require("daisyui")],
 };
