4775cf3bcc19d73ad9e968fd15d96d85d6a82477
Md Ataullah <ataullahm100@gmail.com>
2024-05-10 18:18:29 +0600

    Toggle

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../../assets/finalLogo.png';
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 import { RiMenuAddFill } from 'react-icons/ri';
 import { HiPlus } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 const Nav = () => {
+  // Dark mood Light mood implement
   const [theme, setTheme] = useState(
     localStorage.getItem('theme') ? localStorage.getItem('theme') : 'system'
   );
@@ -34,7 +35,6 @@ const Nav = () => {
       element.classList.remove('dark');
     }
   }
-
   useEffect(() => {
     switch (theme) {
       case 'dark':
@@ -65,21 +65,21 @@ const Nav = () => {
     <div className="fixed top-0 left-0 z-30 w-full bg-[#00000039] py-2">
       <div className="w-11/12 mx-auto flex items-center justify-between">
         <div>
-          <img src={logo} alt="" className="h-16" />
+          <img src={logo} alt="" className="h-12 md:h-16" />
         </div>
         <ul className="hidden md:flex items-center justify-around gap-3 mx-auto">
-          <Link
+          <NavLink
             className="border-2 w-24 py-1.5 text-center text-white hover:border-mainColor duration-200"
             to={'/'}
           >
             Home
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             className="border-2 w-24 py-1.5 text-center text-white hover:border-mainColor duration-200"
             to={'/about'}
           >
             Queries
-          </Link>
+          </NavLink>
           <div className="group relative cursor-pointer py-1.5 w-34">
             <div className="flex items-center justify-between pl-2 lg:pl-0 pr-2 border-2 w-full py-1.5 text-center text-white hover:border-mainColor duration-200">
               <a
@@ -93,7 +93,7 @@ const Nav = () => {
               </span>
             </div>
 
-            <div className="invisible absolute z-50 flex w-60 flex-col bg-gray-100 shadow-4xl group-hover:visible text-center">
+            <div className="invisible absolute z-50 flex w-60 flex-col bg-gray-100 shadow-4xl group-hover:visible text-center dropdownMenu">
               <Link className="text-black hover:bg-mClr py-1.5 hover:text-white font-semibold border-b">
                 Recommendations For Me
               </Link>
@@ -111,22 +111,22 @@ const Nav = () => {
               </Link>
             </div>
           </div>
-          <Link
+          <NavLink
             className="hidden lg:block border-2 w-24 py-1.5 text-center text-white hover:border-mainColor duration-200"
             to={'/about'}
           >
             About Us
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             className="hidden lg:block border-2 w-24 py-1.5 text-center text-white hover:border-mainColor duration-200"
             to={'/about'}
           >
             Contact
-          </Link>
+          </NavLink>
         </ul>
         <div className="hidden md:flex items-center gap-2">
           <div className="relative w-10 h-10">
-            <div className="absolute">
+            <div className="absolute text-white">
               <div className="group flex flex-col items-center justify-center w-max mx-auto">
                 {/* + icon  */}
                 <div className="flex justify-center w-10 h-10 bg-mainColor rounded-full items-center group-hover:rotate-[135deg] hover:bg-[#0095FF]/80 duration-500 text-3xl">
@@ -161,25 +161,48 @@ const Nav = () => {
             </div>
           </div>
           <Link to={'/login'}>
-            <button className="py-2 w-28 text-lg bg-mainColor text-white font-bold">
+            <button className="hover:-skew-x-12 duration-500 py-2 w-28 text-lg bg-mainColor text-white font-bold">
               Login
             </button>
           </Link>
         </div>
 
         {/* Responsive  */}
-        <div className="flex items-center md:hidden">
-          <div className="text-white">
-            <span>
-              <svg
-                className="swap-on fill-current w-8 h-8"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-              </svg>
-            </span>
-            <span></span>
+        <div className="flex items-center gap-2 md:hidden">
+          <div className="relative w-8 h-8">
+            <div className="absolute text-white">
+              <div className="group flex flex-col items-center justify-center w-max mx-auto">
+                {/* + icon  */}
+                <div className="flex justify-center w-8 h-8 bg-mainColor rounded-full items-center group-hover:rotate-[135deg] hover:bg-[#0095FF]/80 duration-500 text-2xl">
+                  <HiPlus />
+                </div>
+                {/* icon container  */}
+                <div className=" text-white text-3xl duration-500 h-0 group-hover:my-4 group-hover:h-full bg-slate-800">
+                  {/* Icon Map */}
+                  {moods?.map((opt, idx) => (
+                    <div
+                      key={idx}
+                      onClick={() => setTheme(opt.text)}
+                      className={`${
+                        theme === opt.text && 'text-white bg-mainColor'
+                      } w-10 h-10 flex items-center justify-center scale-0 group-hover:scale-100 duration-300 shadow-[0px_2px_8px_0px_rgba(99,99,99,0.4)] opacity-0 group-hover:opacity-100 ${
+                        idx === 0
+                          ? 'delay-[400ms] group-hover:delay-100'
+                          : idx === 1
+                          ? 'delay-300 group-hover:delay-200'
+                          : idx === 2
+                          ? 'delay-200 group-hover:delay-300'
+                          : idx === 3
+                          ? 'delay-100 group-hover:delay-[400ms]'
+                          : 'delay-[400ms] group-hover:delay-100'
+                      }`}
+                    >
+                      <ion-icon name={opt.icon}></ion-icon>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
           <div className="group relative cursor-pointer py-1.5 w-34">
             <div className="flex items-center justify-between pl-2 lg:pl-0 pr-2 w-full py-1.5 text-center text-white duration-200">
