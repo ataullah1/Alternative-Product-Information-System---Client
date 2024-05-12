327870f5b9c55a8d8bb431a3e5f3c946c2c2cade
Md Ataullah <ataullahm100@gmail.com>
2024-05-12 13:52:27 +0600

    TanStack Query setup complite

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import { RouterProvider } from 'react-router-dom';
 
 import router from './Route/Routes';
 import Provider from './provider/Provider';
+// TanStack query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
