e2414e5a81e6968b4b4e25365df34eba6d8c0060
Md Ataullah <ataullahm100@gmail.com>
2024-05-12 19:23:58 +0600

    My Query page manage post adding

--- src/pages/MyQueries/MyQuery.jsx
+++ src/pages/MyQueries/MyQuery.jsx
@@ -1,18 +1,13 @@
-import { PiShareFatBold } from 'react-icons/pi';
-import { FaRegCommentDots } from 'react-icons/fa';
+import { FaExternalLinkAlt, FaRegCommentDots } from 'react-icons/fa';
 import { RiHandHeartLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
-import { HiDotsVertical } from 'react-icons/hi';
-import { useState } from 'react';
 import { MdDeleteForever } from 'react-icons/md';
 import { TbEdit } from 'react-icons/tb';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
-import { IoCloseSharp } from 'react-icons/io5';
+import { Tooltip } from '@mui/material';
 
 const MyQuery = ({ dta, handleDelete }) => {
-  const [menu, setMenu] = useState(false);
-
   return (
     <div
       className="mx-auto min-w-full max-w-[500px] rounded-lg bg-white font-sans shadow-lg dark:bg-[#18181B]"
@@ -38,43 +33,28 @@ const MyQuery = ({ dta, handleDelete }) => {
               </div>
             </div>
             {/* Setting button */}
-            <div
-              className="flex cursor-pointer flex-col gap-2 rounded-full text-stone-900 dark:text-white text-2xl py-5 px-2"
-              onClick={() => setMenu(!menu)}
-            >
-              {menu ? (
-                <span>
-                  <IoCloseSharp />
-                </span>
-              ) : (
-                <span>
-                  <HiDotsVertical />
-                </span>
-              )}
-            </div>
-            {menu && (
-              <div className="absolute top-16 right-8 bg-slate-800 dark:bg-slate-700 rounded-md flex flex-col z-30 text-lg text-white">
-                <button
-                  className="py-2 pl-6 pr-10 hover:bg-mClr hover:text-white flex items-center gap-2 rounded-t-md"
-                  onClick={() => setMenu(!menu)}
-                >
-                  <span className="text-2xl">
-                    <AiOutlineInfoCircle />
-                  </span>
-                  Details
-                </button>
-                <button
-                  className="py-2 pl-6 pr-10 hover:bg-mClr hover:text-white flex items-center gap-2"
-                  onClick={() => setMenu(!menu)}
-                >
-                  <span className="text-2xl">
-                    <TbEdit />
-                  </span>
-                  Update
-                </button>
-                <div onClick={() => setMenu(!menu)} className="">
+            <div className="flex items-center gap-1 text-lg font-semibold text-slate-800 dark:text-white/90">
+              <div className="relative group flex flex-col items-center justify-center w-max mx-auto">
+                {/* + icon  */}
+                <div className="flex justify-center w-10 h-10 rounded-full items-center duration-500 text-xl">
+                  <FaExternalLinkAlt />
+                </div>
+                {/* icon container  */}
+                <div className="absolute bottom-6 right-0 text-white duration-500 h-0 group-hover:h-[133px] hidden group-hover:block bg-slate-800 rounded-md">
+                  <button className="shadow-[0px_2px_8px_0px_rgba(99,99,99,0.4)] py-2 pl-6 pr-10 hover:bg-mClr hover:text-white flex items-center gap-2 rounded-t-md">
+                    <span className="text-2xl">
+                      <AiOutlineInfoCircle />
+                    </span>
+                    Details
+                  </button>
+                  <button className="shadow-[0px_2px_8px_0px_rgba(99,99,99,0.4)] py-2 pl-6 pr-10 hover:bg-mClr hover:text-white flex items-center gap-2">
+                    <span className="text-2xl">
+                      <TbEdit />
+                    </span>
+                    Update
+                  </button>
                   <button
-                    className="py-2 pl-6 pr-10 hover:bg-sClr hover:text-white flex items-center gap-2 w-full rounded-b-md"
+                    className="shadow-[0px_2px_8px_0px_rgba(99,99,99,0.4)] py-2 pl-6 pr-10 hover:bg-sClr hover:text-white flex items-center gap-2 w-full rounded-b-md"
                     onClick={() => handleDelete(dta._id)}
                   >
                     <span className="text-2xl">
@@ -84,7 +64,7 @@ const MyQuery = ({ dta, handleDelete }) => {
                   </button>
                 </div>
               </div>
-            )}
+            </div>
           </div>
           {/* Post Image */}
           <div className="flex flex-col gap-1">
@@ -118,34 +98,58 @@ const MyQuery = ({ dta, handleDelete }) => {
           </div>
         </div>
         {/* icons */}
-        <div className="mt-4 flex justify-between px-4 pb-4">
-          <Link
-            to={'/query-details'}
-            className="flex items-center gap-2 text-lg font-semibold text-slate-800 dark:text-white/90"
-          >
-            <span className="text-2xl">
-              <RiHandHeartLine />
-            </span>
-            <h2 className="">40K</h2>
-          </Link>
-          <Link
-            to={'/query-details'}
-            className="flex items-center gap-2 text-lg font-semibold text-slate-800 dark:text-white/90"
-          >
-            <span className="text-2xl">
-              <FaRegCommentDots />
-            </span>
-            <h2 className="">40</h2>
-          </Link>
-          <Link
-            to={'/query-details'}
-            className="flex items-center gap-1 text-lg font-semibold text-slate-800 dark:text-white/90"
-          >
-            <span className="text-2xl">
-              <PiShareFatBold />
-            </span>
-            <h2 className="">40</h2>
-          </Link>
+        <div className="mt-4 flex justify-between px-8 pb-4">
+          <Tooltip title="Support">
+            <button className="flex items-center gap-2 text-lg font-semibold text-slate-800 dark:text-white/90">
+              <span className="text-2xl">
+                <RiHandHeartLine />
+              </span>
+              <h2 className="">40K</h2>
+            </button>
+          </Tooltip>
+          <Tooltip title="Recommendation">
+            <Link
+              to={'/query-details'}
+              className="flex items-center gap-2 text-lg font-semibold text-slate-800 dark:text-white/90"
+            >
+              <span className="text-2xl">
+                <FaRegCommentDots />
+              </span>
+              <h2 className="">40</h2>
+            </Link>
+          </Tooltip>
+          <div className="flex items-center gap-1 text-lg font-semibold text-slate-800 dark:text-white/90">
+            <div className="relative group flex flex-col items-center justify-center w-max mx-auto">
+              {/* + icon  */}
+              <div className="flex justify-center w-10 h-10 rounded-full items-center duration-500 text-xl">
+                <FaExternalLinkAlt />
+              </div>
+              {/* icon container  */}
+              <div className="absolute bottom-6 right-0 text-white duration-500 h-0 group-hover:h-[133px] hidden group-hover:block bg-slate-800 rounded-md">
+                <button className="shadow-[0px_2px_8px_0px_rgba(99,99,99,0.4)] py-2 pl-6 pr-10 hover:bg-mClr hover:text-white flex items-center gap-2 rounded-t-md">
+                  <span className="text-2xl">
+                    <AiOutlineInfoCircle />
+                  </span>
+                  Details
+                </button>
+                <button className="shadow-[0px_2px_8px_0px_rgba(99,99,99,0.4)] py-2 pl-6 pr-10 hover:bg-mClr hover:text-white flex items-center gap-2">
+                  <span className="text-2xl">
+                    <TbEdit />
+                  </span>
+                  Update
+                </button>
+                <button
+                  className="shadow-[0px_2px_8px_0px_rgba(99,99,99,0.4)] py-2 pl-6 pr-10 hover:bg-sClr hover:text-white flex items-center gap-2 w-full rounded-b-md"
+                  onClick={() => handleDelete(dta._id)}
+                >
+                  <span className="text-2xl">
+                    <MdDeleteForever />
+                  </span>
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
